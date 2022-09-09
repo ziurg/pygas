66242109bd9e8d06137d9b641deefd84f6821b68
Guillaume Ruiz <guillaume.ruiz@ingeliance.com>
2022-09-09 08:53:16 +0200

    init project architecture

---
+++ pygas/main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+from model.node import Node
+from model.edge import Edge
+from model.network import Network
+
+
+if __name__ == "__main__":
+    pass
---
+++ pygas/solver/solver.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+
+class Solver:
+    pass
