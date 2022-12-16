80e87bacef46fc9775889b5722f9fbbb32d15ea5
Guillaume Ruiz <contact.gruiz@gmail.com>
2022-12-17 00:01:44 +0100

    Modification de la structure de Node pour mixer __setattr__ et @property

--- app/domain/model/node.py
+++ app/domain/model/node.py
@@ -1,54 +1,76 @@
 # -*- coding: utf-8 -*-
-from dataclasses import dataclass
 
-
-@dataclass
 class Node:
-    """Node object
-
-    Note
-    ----
-    Other attributes used for network are :
-    label: str
-        name of the node
-    x: float
-        x coordinate of the node
-    y: float
-        y coordinate of the node
-    z: float
-        z coordinate of the node
-    pressure: float
-        nodal pressure
-    flow: float
-        flow consumption at the node
-    active: bool
-        can be inactive in case of special nodes (customer or valve)
-    """
-
     def __init__(self, id, **kwargs):
         self.id = id
-        self.pressure = 0.0
-        self.flow = 0.0
-        self.fixed_pressure = None
-        self.fixed_flow = None
+        self._pressure = 0.0
+        self._flow = 0.0
+        self._fixed_pressure = None
+        self._fixed_flow = None
         self.params = {}
         for k, v in kwargs.items():
-            try:
-                self.__dict__[k] = v
-            except KeyError:
-                self.params[k] = v
+            self.__setattr__(k, v)
+
+    @property
+    def fixed_pressure(self):
+        return self._fixed_pressure
+        
+    @fixed_pressure.setter
+    def fixed_pressure(self, value):
+        if self._fixed_flow is None:
+            self._fixed_pressure = value
+            self._pressure = value
+        else:
+            raise Exception("Can't get node with both fixed pressure and flow.")
+
+
+    @property
+    def pressure(self):
+        return self._pressure
+        
+    @pressure.setter
+    def pressure(self, value):
+        if self._fixed_pressure is None:
+            self._pressure = value
+
+    @property
+    def fixed_flow(self):
+        return self._fixed_flow
+        
+    @fixed_flow.setter
+    def fixed_flow(self, value):
+        if self._fixed_pressure is None:
+            self._fixed_flow = value
+            self._flow = value
+        else:
+            raise Exception("Can't get node with both fixed pressure and flow.")
+
+    @property
+    def flow(self):
+        return self._flow
+        
+    @flow.setter
+    def flow(self, value):
+        if self._fixed_flow is None:
+            self._flow = value
 
     def __getattr__(self, attribute):
         try:
-            return self.params[attribute]
-        except KeyError:
             return self.__dict__[attribute]
+        except KeyError:
+            return self.params[attribute]
 
     def __setattr__(self, attr_name, attr_value):
-        try:
-            self.__dict__[attr_name] = attr_value
-        except KeyError:
-            self.params[attr_name] = attr_value
+        if attr_name in ["fixed_pressure", "fixed_flow", "flow", "pressure"]:
+            super(Node, self).__setattr__(attr_name, attr_value)
+        else:
+            try:
+                self.__dict__[attr_name] = attr_value
+            except KeyError:
+                self.params[attr_name] = attr_value
+
+    def __str__(self):
+        return f'Node {self.id} (Q={self.flow:.2f}; P={self.pressure:.2f})'
 
     def __eq__(self, other: int):
         return self.id == other
